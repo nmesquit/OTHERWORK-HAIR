b5201efe0c406b675bc78442d2c363466a0a2988
nmesquit <68023464+nmesquit@users.noreply.github.com>
2026-08-24 17:22:35 -0700

    Delete OTHERWORK-HAIR directory

--- OTHERWORK-HAIR/public/sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE='otherwork-v1';
-const ASSETS=['/','/styles.css','/app.js','/manifest.webmanifest','/icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
